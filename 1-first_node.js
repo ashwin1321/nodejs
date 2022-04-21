63d37f2dd9f9aaaa890095e8d74dfecb0feef34c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-04-21 23:19:28 +0545

    globals

---
+++ 1-first_node.js
@@ -0,0 +1,10 @@
+const amount = 12
+
+if (amount < 10) {
+    console.log("small number");
+
+} else {
+    console.log("big number");
+}
+
+console.log("hey , get a life");
---
+++ 2-global.js
@@ -0,0 +1,17 @@
+// GLOBAL VARIABLES
+
+// __dirnmae    - the name of the current directory
+// __filename   - the name of the current file
+// require      - a function that can be used to load other modules (CommoonJS)
+// module       - info about the current module (file)
+// process      - info about the env where the program is runnings
+
+console.log(__dirname);
+
+setInterval(() => {
+    console.log("hello world");
+}, 1000);
+
+// setTimeout(() => {
+//     console.log("bye world");
+// }, 2000);
