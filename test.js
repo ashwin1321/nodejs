c8f60e3313c5524bfe6fcf1cf46ac900722becca
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-04-24 21:41:49 +0545

    packages

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+// creating the package
+
+// npm init -- step by step process
+// npm init -y   -- shortcut
+
+// installing other dependencies npm creates a folder node_modules which has all the dependencies inside 
+
+const _ = require('lodash');
+
+const item = [1,[2,[3,[4]]]];
+const newitem = _.flattenDeep(item);
+console.log(newitem);
