2e097bfd14e5bd714130f87e885604237fe8bd9a
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-04-24 22:30:49 +0545

    update

--- test.js
+++ test.js
@@ -3,7 +3,12 @@
 // npm init -- step by step process
 // npm init -y   -- shortcut
 
+//installing package locally ---npm i <packagename> 
+//installing package globally ---npm install -g <packagename> 
+//installing package as dev developer ---npm i <packagename> -D
+
 // installing other dependencies npm creates a folder node_modules which has all the dependencies inside 
+// uninstall the dependencies by running --  npm uninstall <package name>
 
 const _ = require('lodash');
 
