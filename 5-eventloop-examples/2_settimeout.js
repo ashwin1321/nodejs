3cc825b7baabc2b7042847fff62d6f425e2467e6
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-04-28 23:12:02 +0545

    eventloops

---
+++ 5-eventloop-examples/2_settimeout.js
@@ -0,0 +1,9 @@
+// starting operating the process
+
+console.log("first");
+setTimeout(()=>{
+    console.log('second');
+},0)
+console.log('third');
+// completer and exited operating system process
+
---
+++ 5-eventloop-examples/3_setinterval.js
@@ -0,0 +1,8 @@
+setInterval(()=>{
+    console.log('hey hey');
+
+},2000)
+// setTimeout(() => {
+//     console.log('mf');
+// }, 6000);
+console.log('this runs first');
