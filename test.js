3bcb23fdfa3cb4485f2cfd1c598e6e0efb7b23c6
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-05-03 21:16:00 +0545

    changes

--- test.js
+++ test.js
@@ -1,22 +1,11 @@
-// http request object
-
 const http = require('http');
-const server = http.createServer((req, res) => {
-    // console.log(req.method);           // get or post
-    // console.log(req.url);           
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.write('<h1>Hello World</h1>');
+const {readFileSync} = require('fs');
 
-    if(req.url == '/') {
-        res.write('<h1>Home page ho la</h1>');
-    }
-    else if (req.url == '/about'){
-        res.write('<h1>About page ho la</h1>');
-    }
-    else{
-        res.write('<h1>kina hero</h1>');
-    }
-    res.end();
-})
+// get all files
+const homePage = readFileSync('./index.html');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(homePage);
+});
 
 server.listen(5000);
