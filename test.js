74d02c568aec7118a4d0df456a0b2329fad046e5
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-05-04 21:59:15 +0545

    changes

--- test.js
+++ test.js
@@ -1,14 +1,1 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
-app.post('/user', (req, res) => {
-  res.send('Create User')
-})
-app.all('*', (req, res) => {
-  res.status(404).send('<h1> route not found </h1>')
-})
-app.listen(3000)
+// aaja kei padhena yaar xya
