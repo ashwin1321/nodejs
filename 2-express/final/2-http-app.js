2f5db210eeebc92f7a95d81738962b0380e6c74c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-05-05 19:45:38 +0545

    http-app

---
+++ 2-express/final/2-http-app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// setup static and middleware
+app.use(express.static('../public')) // serve static files from public folder    /
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../navbar app/index.html')) // sending html files
+// })
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>404</h1> <h1>resource Not Found!</h1>')
+})
+
+app.listen(4000, (req, res) => {
+  console.log(`server is listening on port 4000.`)
+})
