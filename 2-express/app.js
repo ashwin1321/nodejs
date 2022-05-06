6b8ded81e1fb49f7b4762a252a2601914c5b4513
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-05-06 15:26:51 +0545

    aauyush

--- 2-express/app.js
+++ 2-express/app.js
@@ -4,10 +4,40 @@ const app = express()
 const { products } = require('./data')
 
 app.get('/', (req, res) => {
-  //   res.json([{ name: 'john' }, { name: 'jane' }])
-  res.json(products)
+  res.send('<h1> Home page </h1><A href="/api/products">Products</a>')
 })
 
-app.listen(3000, () => {
-  console.log('Server starting at port 3000')
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product
+    return { id, name, image }
+  })
+  res.json(newProducts)
+})
+
+//      // For each id we may use the following code but not feasible for bigger data
+
+// app.get('/api/products/1', (req, res) => {
+//   const singleProduct = products.find((product) => product.id === 1) // query
+
+//   res.json(singleProduct)
+// })
+
+//     // more of a dynamic approachs
+
+app.get('/api/products/:productID', (req, res) => {
+  // console.log(req.params)
+  const { productID } = req.params
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID),
+  ) // query
+  // console.log(singleProduct)
+  if (!singleProduct) {
+    return res.status(404).send('product does not exists')
+  }
+  res.json(singleProduct)
+})
+
+app.listen(2000, () => {
+  console.log('Server starting at port 2000')
 })
--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-// aaja kei padhena yaar xya
+const express = require('express')
+
+const app = express()
+const { products } = require('../data')
+
+app.get('/', (req, res) => {
+  res.send('<h1> Home page </h1><A href="/api/products">Products</a>')
+})
+
+app.listen(3000, () => {
+  console.log('Server starting at port 3000')
+})
